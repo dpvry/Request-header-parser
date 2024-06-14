7fabb79a57d4d13d46ae29a1ae5f06d65c7adc96
Deep V <diipp5252@gmail.com>
2024-06-14 15:41:52 +0000

    first commit

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ip = req.ip;
+    const lang = req.headers['accept-language'];
+    const soft = req.headers['user-agent'];
+    console.log(ip,lang,soft);
+  res.json({ ipaddress:ip,language:lang,software:soft});
 });
 
 // listen for requests :)
